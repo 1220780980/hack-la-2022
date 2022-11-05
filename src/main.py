8092599a7ab50c3f46465a2f67e37ec04de5e698
Tianhong Zhou <teozhou1215@gmail.com>
2022-11-05 13:30:08 -0700

    Edit home page of the website Add Enrollment page of the website

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pandas
+from matplotlib import pyplot as plt
 
 navigation_events = pandas.read_csv("../data/navigation_events.csv")
 assignments = pandas.read_csv("../data/additional/assignments.csv")
@@ -10,7 +11,18 @@ gradebook = pandas.read_csv("../data/additional/gradebook.csv")
 module_items = pandas.read_csv("../data/additional/module_items.csv")
 pages = pandas.read_csv("../data/additional/pages.csv")
 
-print(module_items.sort_values(by=["module_position"]))
-# print(gradebook.sort_values(by=["Student"])["Student"])
-# print(navigation_events["event__object_extensions_asset_name"])
+total_time = enrollments.sort_values(by=["user_id"]).drop(index=34)["total_activity_time"]
+score = gradebook.sort_values(by=["Student"]).drop(index=0).drop(index=1)["Current Score"]
+# for i in range(len(score)):
+#     score.iloc[i] = float(score.iloc[i])
+# plt.scatter(total_time, score)
+# plt.xlabel("Total Activity Time")
+# plt.ylabel("Current Score")
+# plt.title("Total Activity Time vs Current Score")
+for i in range(score.shape[0]):
+    print(total_time.iloc[i])
+# print(enrollments.sort_values(by=["user_id"])["total_activity_time"])
+# print(enrollments.sort_values(by=["user_id"]).drop(index=34))
+# print(gradebook.sort_values(by=["Student"]).drop(index=0).drop(index=1)["Current Score"])
+
 
